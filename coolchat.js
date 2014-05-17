943cc1612bd45d6c3c570cf22d642f898ac88a28
afbresley <afbresley@gmail.com>
2014-05-17 16:53:17 -0400

    more stylings, and gets scrolling to work

--- coolchat.js
+++ coolchat.js
@@ -1,31 +1,46 @@
 var Chats = new Meteor.Collection("chats");
 
 if (Meteor.isClient) {
+
+  var username = function () {
+    return Session.get("username") || "Anomymous";
+  };
+
   Meteor.startup(function () {
     Session.set("loggedIn", Date.now());
   });
 
-  Template.chatRoom.chats = function () {
-    return Chats.find({time_created: {$gt: Session.get("loggedIn")}});
-  };
-
-  Template.chatRoom.username = function() {
-    return Session.get("username");
-  };
-
   Template.chatRoom.events({
     "submit form#username": function(event) {
       event.preventDefault();
+
       var username = $("input#username").val();
       Session.set("username", username);
     },
 
     "click .reset-username": function(event) {
       event.preventDefault();
+
       Session.set("username", "");
     }
-
   });
+
+  Template.chatRoom.chats = function () {
+    return Session.get("username");
+  };
+
+  Template.chat.rendered = function () {
+    var $chats = $("ul#chats");
+    var $lastChat = $("ul#chats>li.chat:last-child");
+    if ($lastChat && $lastChat.position()) {
+      console.log("i suck");
+      $chats.scrollTop($lastChat.position().top);
+    }
+  };
+
+  Template.chatRoom.chats = function () {
+    return Chats.find({time_created: {$gt: Session.get("loggedIn")}});
+  };
 
   Template.newMessage.events({
     "submit form.new-message": function(event) {
@@ -34,23 +49,10 @@ if (Meteor.isClient) {
       var $message= $("input#message");
       Chats.insert({
         message: $message.val(),
-        username: Session.get("username"),
+        username: username(),
         time_created: Date.now()
-      })
+      });
       $message.val("");
-    }
-  });
-
-
-
-  Chats.find().observeChanges({
-    added: function(id, fields) {
-      var $chats = $("ul#chats");
-      var $lastChat = $("ul#chats>li.chat:last-child");
-      if ($lastChat && $lastChat.position()) {
-        $chats.scroll();
-        // $chats.scrollTop($lastChat.position().top + $lastChat.height());
-      }
     }
   });
 }
